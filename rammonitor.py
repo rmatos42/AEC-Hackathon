d0e01ebb104be823e258a4c3b25af648c784b318
rmatos <rmatos@Levins-MacBook-Air-2.local>
2017-07-23 11:39:59 -0700

    added some stuff

--- rammonitor.py
+++ rammonitor.py
@@ -8,7 +8,8 @@ from subprocess import call
 old_vms = 0
 log = open("log.log", "w")
 csv = open("log.csv", "w")
-csv.write("hostname,timestamp,percent_change,unresponsive_time,total_time\n")
+time_unit = 1
+csv.write("hostname,timestamp,percent_change,unresponsive,unresponsive_time,time_unit,total_time\n")
 i = 0
 time_step = 1
 unresponsive = 0
@@ -22,12 +23,14 @@ while pid == 0:
 	time.sleep(1)
 
 while (1):
+	unresp = 0
 	process = psutil.Process(pid)
 	vms = process.memory_info().vms
 	timing = datetime.datetime.now()
 	diff_vms = vms - old_vms
 	percent_vms = (float(diff_vms) / vms) * 100
 	if (percent_vms > threshold and i > 0):
+		unresp = 1
 		unresponsive += 1
 	if (i % time_step == 0):
 		print (timing)
@@ -46,8 +49,8 @@ while (1):
 	log.write("percent_vms: {0}%\n\n".format(percent_vms))
 	log.write("total unresponsive seconds: {0}".format(unresponsive))
 	log.write("total seconds: {0}".format(i))
-	csv.write("{0},{1},{2},{3},{4}\n".format(socket.gethostname(), datetime.datetime.now(), percent_vms, unresponsive, i))
-	time.sleep(1)
+	csv.write("{0},{1},{2},{3},{4},{5},{6}\n".format(socket.gethostname(), datetime.datetime.now(), percent_vms, unresp,  unresponsive, time_unit, i))
+	time.sleep(time_unit)
 	old_vms = vms
 	i += 1
 log.close()
