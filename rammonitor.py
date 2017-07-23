c2da0f09ee46c375fb6b435234809c4124bf190b
all-hack <obelanger@fordham.edu>
2017-07-22 18:29:25 -0700

    robust time step

--- rammonitor.py
+++ rammonitor.py
@@ -7,6 +7,7 @@ import datetime
 old_vms = 0
 log = open("log.log", "a")
 i = 0
+time_step = 10
 
 while (1):    
     process = psutil.Process(int(sys.argv[1]))
@@ -14,14 +15,14 @@ while (1):
     timing = datetime.datetime.now()
     diff_vms = vms - old_vms
     percent_vms = (float(diff_vms) / vms) * 100
-    if (i % 10 == 0):
+    if (i % time_step == 0):
         print (timing)
     print ("vms: {0}".format(vms))
     print ("old_vms: {0}".format(old_vms))
     print ("diff_vms: {0}".format(diff_vms))
     print ("percent_vms: {0}%".format(percent_vms))
     print ("")
-    if (i % 10 == 0):
+    if (i % time_step == 0):
         log.write("{0}\n".format(timing))
     log.write("vms: {0}\n".format(vms))
     log.write("old_vms: {0}\n".format(old_vms))
