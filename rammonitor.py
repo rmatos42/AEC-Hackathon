6b1df61639e027cf2e9c0cf607fd4a0cc01b5652
rmatos <rmatos@Levins-MacBook-Air-2.local>
2017-07-22 19:14:36 -0700

    added total seconds

--- rammonitor.py
+++ rammonitor.py
@@ -30,6 +30,7 @@ while (1):
     print ("diff_vms: {0}".format(diff_vms))
     print ("percent_vms: {0}%".format(percent_vms))
     print ("total unresponsive seconds: {0}".format(unresponsive))
+    print ("total seconds: {0}".format(i))
     print ("")
     if (i % time_step == 0):
         log.write("{0}\n".format(timing))
@@ -38,6 +39,7 @@ while (1):
     log.write("diff_vms: {0}\n".format(diff_vms))
     log.write("percent_vms: {0}%\n\n".format(percent_vms))
     log.write("total unresponsive seconds: {0}".format(unresponsive))
+    log.write("total seconds: {0}".format(i))
     time.sleep(1)
     old_vms = vms
     i += 1
